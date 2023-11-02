a2fcafe368186ff8b2e8472680289049f61c697e
Shailendra Kushwaha <learning4succedding@gmail.com>
2023-11-02 08:18:15 +0200

    javascript all

---
+++ variables.js
@@ -0,0 +1,40 @@
+/*
+#Notes
+Link - https://github.com/hiteshchoudhary/js-hindi-youtube/tree/main/01_basics
+
+-Variables are used to store information.
+-It is a named storage for data.
+-A varable name should be descriptive as per the data it is storing. 
+-In real project, most of the time is spent modifying and extending an existing code base rather than writing something completely separate from scratch.
+*/
+
+/*
+Task 1.
+Declare two variable: admin and name.
+Assign the value "John" to "name". 
+Copy the value from name to admin. 
+Show the value of admin using alert (must output "John")
+
+let admin;
+let name;
+
+name = "John"
+admin = name;
+
+alert(admin);
+
+Task 2:
+1. Create a variable with the name of our planet. How would you name a such a variable?
+2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+const ourPlanet = "earth";
+let currentVisitor = "John";
+
+*/
+
+
+
+
+
+
+
