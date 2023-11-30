6aaffc06037c5ec076a57faa6afe05acc10e8326
Shailendra Kushwaha <learning4succedding@gmail.com>
2023-11-30 06:02:28 +0200

    changes made

--- Basics/05_strings.js
+++ Basics/05_strings.js
@@ -1,10 +1,10 @@
 // String basics
-const city = "Helsinki";
+const area = "Hakaniemenranta";
 const postCode = 530;
 
 //Using back-tick `` in contatenate strings.
 
-console.log(`The ${city} postal code is ${postCode}.`)
+console.log(`The ${area} postal code is ${postCode}.`)
 
 //String object constructor and Strings methods
 const courseName = new String("FullStack");
@@ -21,7 +21,7 @@ console.log(newString);
 //slice method
 const sliceString = courseName.slice(0,5);
 console.log(sliceString);
-const sliceFromBack = courseName.slice(-9,3);
+const sliceFromBack = courseName.slice(-8,3);
 console.log(sliceFromBack);
 
 //Trim method
@@ -29,3 +29,8 @@ console.log(sliceFromBack);
 const stringTrim = "     Software Developer     ";
 console.log(stringTrim);
 console.log(stringTrim.trim());
+
+//Replace method
+const url = "https://shailendra.com/shailendra%20kushwaha";
+const change = url.replace("%20", "-");
+console.log(change);
